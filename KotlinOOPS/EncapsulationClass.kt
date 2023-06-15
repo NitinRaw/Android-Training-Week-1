492c112754c7de80d8670bb3f131ab3dbe7cb21a
Nitin Pandey <nitin.pandey@Nitins-MacBook-Pro.local>
2023-06-15 13:03:51 +0530

    removed zips

---
+++ KotlinOOPS/EncapsulationClass.kt
@@ -0,0 +1,55 @@
+
+
+
+
+class EncapsulationClass {
+
+    private var name = "Nitin"
+    internal val age = 15
+    protected val value1 = "Value1"
+    val temp = "Temp"
+
+    fun getMyName() : String{
+        return name
+    }
+
+    fun setMyName(name : String){
+        this.name = name
+    }
+
+    protected class Nested{
+        val p : Int = 36
+    }
+
+
+}
+
+//
+//class A: EncapsulationClass(){
+//    val a = value1
+////    val b = name  Not allowing
+//    val c = age
+//
+//   val d = Nested().p
+//
+//}
+
+fun main(){
+    val obj =EncapsulationClass()
+    obj.setMyName("s")
+    println(obj.getMyName())
+
+
+
+
+
+
+    println(obj.age)
+    println(obj.getMyName())
+    println(obj.setMyName("Nick"))
+    println(obj.getMyName())
+
+    val obj1 =EncapsulationClass()
+    println(obj1.getMyName())
+
+}
